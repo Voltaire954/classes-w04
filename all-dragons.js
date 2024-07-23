2372079b9e1f1a214140f6b2b974996f43e71be0
christopher <voltaire.christopher@yahoo.com>
2024-07-23 18:16:53 -0400

    chris drivin done

--- all-dragons.js
+++ all-dragons.js
@@ -1,12 +1,48 @@
-// Your code here 
+// Your code here
+class FriendlyDragon {
+  constructor(name, color, lifeGoals, friend) {
+    this.name = name;
+    this.color = color;
+    this.lifeGoals = lifeGoals;
+    this.friend = friend;
+  }
 
+}
+class EvilDragon {
+  constructor (name, color, evilDoings, nemesis){
+    this.name = name
+    this.color = color
+    this.evilDoings = evilDoings
+    this.nemesis = nemesis
+  }
+}
+const smaug = new EvilDragon (
+  'Smaug',
+  'black',
+  [
+    "take over your mountain kingdom",
+    "steal all your dwarven gold",
+    "burn down your floating village",
+  ],
+  'Dwarf King'
+)
+const falkor = new FriendlyDragon(
+  "Falkor",
+  "white",
+  [
+    "save Atreyu from the swamp",
+    "save Atreyu from the Nothing",
+    "scare the local bullies into a dumpster",
+  ],
+  "Bastian"
+);
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports.falkor = falkor
-  module.exports.smaug = smaug
+  module.exports.falkor = falkor;
+  module.exports.smaug = smaug;
 } catch {
   module.exports = null;
 }
